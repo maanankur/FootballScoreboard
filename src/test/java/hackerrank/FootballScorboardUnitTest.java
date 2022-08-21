016377051ae7ec0b9bfb0f9b580a497dc4771ed1
Ankur Maan <c-ankur.maan@metricstream.com>
2022-08-21 20:43:28 +0530

    added first test case for start a game operation

---
+++ src/test/java/hackerrank/FootballScorboardUnitTest.java
@@ -0,0 +1,35 @@
+package hackerrank;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+/**
+ * This is the Junit class for football score-board service 
+ * @author ankur.maan
+ */
+@TestMethodOrder(MethodOrderer.Alphanumeric.class)
+public class FootballScorboardUnitTest {
+	private static FootballScoreboardService footballService;
+
+	@BeforeAll
+	public static void instantiate() {
+		footballService = new FootballScoreboardService();
+	}
+
+	/**
+	 * This is the test case for a match with invalid team name
+	 */
+	@Test
+	public void _01_invalidMatchNothing() {
+		assertEquals(0, footballService.getSummary().size());
+	}
+
+}
