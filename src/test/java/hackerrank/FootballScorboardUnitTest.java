ac83c743febb2f218ebeaaa7904192812d82a36f
Ankur Maan <c-ankur.maan@metricstream.com>
2022-08-21 22:32:01 +0530

    refactored implementaton and added a test case for a match with valid team names

--- src/test/java/hackerrank/FootballScorboardUnitTest.java
+++ src/test/java/hackerrank/FootballScorboardUnitTest.java
@@ -34,5 +34,16 @@ public class FootballScorboardUnitTest {
 		footballService.startNewGame(m2);
 		assertEquals(0, footballService.getSummary().size());
 	}
+	
+	/**
+	 * This is the test case for a match with valid team names
+	 */
+	@Test
+	public void _02_validMatch() {
+		FootballMatch m1 = new FootballMatch("Mexico", "Canada");
+		footballService.startNewGame(m1);
+
+		assertEquals(1, footballService.getSummary().size());
+	}
 
 }
